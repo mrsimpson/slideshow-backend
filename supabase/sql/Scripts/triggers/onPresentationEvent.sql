9afbc7d0af354a8e124ce17367a597d0812751eb
Oliver Jägle <github@beimir.net>
2024-06-06 00:26:50 +0200

    Start/stop presentation with triggers

--- supabase/sql/Scripts/triggers/onPresentationEvent.sql
+++ supabase/sql/Scripts/triggers/onPresentationEvent.sql
@@ -2,14 +2,18 @@
 create or replace function handle_presentation_event_inserted()
     returns trigger
     language plpgsql
-as $$
+as
+$$
+declare
+    target_state presentation_lifecycle_status;
 begin
     raise log 'event handling triggered for %', new;
-    if new.type = 'presentation_start' then
-        update postgres.public.presentations set lc_status = 'started' where id = new.presentation;
-    end if;
-    if new.type = 'presentation_stop' then
-        update postgres.public.presentations set lc_status = 'stopped' where id = new.presentation;
+    case new.type
+        when 'presentation_start' then target_state = 'started';
+        when 'presentation_stop' then target_state = 'stopped';
+        end case;
+    if target_state is not null then
+        update postgres.public.presentations set lc_status = target_state where id = new.presentation;
     end if;
     return new;
 end;
@@ -18,6 +22,7 @@ $$;
 -- drop trigger if exists after_presentation_event_inserted on postgres.public.presentation_events;
 
 create or replace trigger after_presentation_event_inserted
-    after insert on postgres.public.presentation_events
+    after insert
+    on postgres.public.presentation_events
     for each row
 execute function handle_presentation_event_inserted();
